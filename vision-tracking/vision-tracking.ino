cc525bd6b5e902176255f0984cfd7c3fb753ef80
Feilan Jiang <fjiang120@gmail.com>
2017-03-02 23:45:29 -0500

    Variable-RPM vision tracking working

--- vision-tracking/vision-tracking.ino
+++ vision-tracking/vision-tracking.ino
@@ -4,6 +4,7 @@
 #define CAM_MAX 500
 #define MAX_RPM 60
 #define STEP_DEG 5
+#define THRESHOLD 30
 
 PVision cam;
 
@@ -12,16 +13,28 @@ long rpm;
 int pos;
 
 void setup() {
+  cam.init();
   Serial.begin(9600);
 }
 
 void loop() {
   if (cam.read() & BLOB1) {
     pos = cam.Blob1.Y - (CAM_MAX / 2);  // ports for X and Y are mixed up - need to fix
-    rpm = ((double) abs(pos) / (CAM_MAX / 2)) * MAX_RPM;
-    motor.setSpeed(rpm);
-    motor.step(pos > 0 ? STEP_DEG: -STEP_DEG);  // which sign is clockwise?
+
+    if (abs(pos) > THRESHOLD) {
+      rpm = ((double) abs(pos) / (CAM_MAX / 2)) * MAX_RPM;
+      motor.setSpeed(rpm);
+      motor.step(pos > 0 ? STEP_DEG: -STEP_DEG);  // which sign is clockwise?
+    }
+
+    Serial.print(cam.Blob1.Y);
+    Serial.print(" ");
+    Serial.print(cam.Blob1.X);
+    Serial.print(" ");
+    Serial.print(cam.Blob1.Size);
+    Serial.println();
   }
 
-  delay(100);
+  Serial.println("---");
 }
+
