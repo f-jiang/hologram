1231e4896d067368af7acbfc5e9bb89729109eb3
Feilan Jiang <fjiang120@gmail.com>
2017-03-19 19:51:05 -0400

    Remove PID

--- hologram-device/hologram-device.ino
+++ hologram-device/hologram-device.ino
@@ -1,14 +1,7 @@
-//#define PID_VISION_TRACKING
 #define PRINT_DBG
 
-#ifdef PID_VISION_TRACKING
 #include <Stepper.h>
 #include <PVision.h>
-#include <PID_v1.h>
-#else
-#include <Stepper.h>
-#include <PVision.h>
-#endif
 
 #define CAM_MAX 760
 #define CAM_MIN 0
@@ -21,19 +14,6 @@
 #define ENC_A  2
 #define ENC_B  3
 
-#ifdef PID_VISION_TRACKING
-PVision cam;
-double pos = 0;
-bool hasBlob;
-
-Stepper motor(360, 8, 9, 10, 11);
-double mult = 0;
-long rpm;
-int step;
-
-double setpoint = CAM_CENT;
-PID pidCtrl(&pos, &mult, &setpoint, 0.1, 0.0, 0.0, DIRECT);
-#else
 bool varRpm = true;
 bool varStep = true;
 
@@ -46,7 +26,6 @@ long rpm;
 int step;
 
 double mult;
-#endif
 
 uint16_t encPos = 0;
 uint16_t camY = 0;
@@ -83,16 +62,8 @@ void doEncoder() {
 }
 
 void setup() {
-#ifdef PID_VISION_TRACKING
   cam.init();
   motor.setSpeed(MAX_RPM);
-
-  pidCtrl.SetMode(AUTOMATIC);
-  pidCtrl.SetOutputLimits(-100, 100);
-#else
-  cam.init();
-  motor.setSpeed(MAX_RPM);
-#endif
 
   pinMode(ENC_A, INPUT); 
   digitalWrite(ENC_A, HIGH);       // turn on pull-up resistor
@@ -105,57 +76,6 @@ void setup() {
 } 
 
 void loop(){
-#ifdef PID_VISION_TRACKING
-  hasBlob = cam.read() & BLOB1;
-
-  if (hasBlob) {
-    pos = cam.Blob1.Y;  // ports for X and Y are mixed up - need to fix
-    camY = cam.Blob1.X;
-    serialWrite();
-
-    if (abs(pos) > DEADBAND) { 
-      pidCtrl.Compute();
-      mult /= 100.0;
-
-      rpm = abs(mult) * MAX_RPM;
-      step = mult * MAX_STEP;
-
-      if (pos > CAM_CENT) {
-        step *= -1;
-      }
-
-//      motor.setSpeed(rpm);
-//      motor.step(step);  // which sign is clockwise?
-    }
-  }
-
-  if (!hasBlob || abs(pos) <= DEADBAND) {
-    digitalWrite(8, LOW);
-    digitalWrite(9, LOW);
-    digitalWrite(10, LOW);
-    digitalWrite(11, LOW);
-  }
-
-#ifdef PRINT_DBG
-  Serial.print(cam.Blob1.Y);
-  Serial.print(" ");
-  Serial.print(pos);
-  Serial.print(" ");
-  Serial.print(cam.Blob1.X);
-  Serial.print(" ");
-  Serial.print(cam.Blob1.Size);
-  Serial.print(" ");
-  Serial.print(setpoint);
-  Serial.print(" ");
-  Serial.print(mult);
-  Serial.print(" ");            
-  Serial.print(rpm);
-  Serial.print(" ");            
-  Serial.print(step);
-  Serial.print(" ");            
-  Serial.println();
-#endif
-#else
   hasBlob = cam.read() & BLOB1;
 
   if (hasBlob) {
@@ -192,6 +112,5 @@ void loop(){
     digitalWrite(10, LOW);
     digitalWrite(11, LOW);
   }
-#endif
 }
 
