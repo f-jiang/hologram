d850ef3b5e03411426ffee5072c5e70e9dcfff14
Feilan Jiang <fjiang120@gmail.com>
2017-03-22 12:41:26 -0400

    Restructure project to allow for gallery slider
    sketchfab and jquery would conflict if html and js in same file, so had so separate the code, install express, etc.

--- model-viewer/index.js
+++ model-viewer/index.js
@@ -1,34 +1,39 @@
 'use strict';
 
 (function() {
+  var http = require('http');
+  var fs = require('fs');
+  var express = require('express');
+  var path = require('path');
+  var SerialPort = require('serialport');
+
   const SERIAL_PORT = process.argv[2];  // node index.js /dev/ttyUSB0
+  const HTTP_PORT = 8080;
+  const APP_DIR = path.join(__dirname, 'frontend');
+
+  var app = express();
+  var server = http.Server(app);
+  var io = require('socket.io')(server);
+  var serial = new SerialPort(SERIAL_PORT, {baudrate: 9600});
 
   var map = function(val, fromLo, fromHi, toLo, toHi) {
     return toLo + (val - fromLo) * (toHi - toLo) / (fromHi - fromLo);
   };
 
-  var http = require('http');
-  var fs = require('fs');
+  app.use(express.static(APP_DIR));
 
-  var server = http.createServer((req, res) => {
-    fs.readFile('./app.html', 'utf-8', function(error, content) {
-      res.writeHead(200, {"Content-Type": "text/html"});
-      res.end(content);
-    });
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(APP_DIR, 'app.html'));
   });
 
-  var io = require('socket.io').listen(server);
-  var SerialPort = require("serialport");
-  var serialPort = new SerialPort(SERIAL_PORT, {baudrate: 9600});
-
-  serialPort.on('open', () => {
+  serial.on('open', () => {
     console.log('opened connection to serial port', SERIAL_PORT);
 
     io.sockets.on('connection', (socket) => {
       console.log('connection with client opened');
 
       var angle, elevation, buf;
-      serialPort.on('data', (data) => {
+      serial.on('data', (data) => {
         buf = Buffer.from(data);
         angle = map(buf.readUInt16LE(0), 0, 320, 0, 2 * Math.PI);
         elevation = map(buf.readUInt16LE(2), 0, 1024, 0, 1);
@@ -39,6 +44,12 @@
     });
   });
 
-  server.listen(8080);
+  server.listen(HTTP_PORT, (err) => {
+    if (err) {
+      console.log('server unable to listen on port', HTTP_PORT);
+    } else {
+      console.log('server listening on port', HTTP_PORT);
+    }
+  });
 })();
 
